f9c5316bb6691cb81bcf08f6e1cf11ffe0212038
Vladislav Gurenko <limeman14@gmail.com>
2020-10-07 01:57:16 +0300

    User entity + liquibase added to project

--- src/main/java/ru/skillbox/blogengine/controller/ApiGeneralController.java
+++ src/main/java/ru/skillbox/blogengine/controller/ApiGeneralController.java
@@ -1,6 +1,5 @@
 package ru.skillbox.blogengine.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/ru/skillbox/blogengine/entity/User.java
+++ src/main/java/ru/skillbox/blogengine/entity/User.java
@@ -1,16 +1,38 @@
 package ru.skillbox.blogengine.entity;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
-
-import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Data
 @Table(name = "users")
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private boolean isModerator;
 
+    @CreationTimestamp
+    private Timestamp regTime;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String code;
+
+    private String photo;
 }
